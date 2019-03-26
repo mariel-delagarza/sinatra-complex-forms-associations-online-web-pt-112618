719f175cc4cc33f8ae7e5f9127cd1951dd6c293f
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-26 21:55:41 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,10 +11,10 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-  @owner = Owner.create(params[:owner])
-  if !params["pet"]["name"].empty?
-    @owner.pets << Pet.create(name: params["pet"]["name"])
-  end
+   @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
     redirect "owners/#{@owner.id}"
   end
 
@@ -30,11 +30,11 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    ####### bug fix
+   
     if !params[:owner].keys.include?("pet_ids")
       params[:owner]["pet_ids"] = []
     end
-    #######
+   
  
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
